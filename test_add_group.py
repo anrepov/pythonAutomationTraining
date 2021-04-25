510cec2ef946889cf936168063f5bc0669f6855c
anrepov <anrepov@gmail.com>
2021-04-25 15:57:00 +0300

    task 2 - refactoring

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 from selenium import webdriver
+
+from group import Group
 
 
 class TestAddGroup(unittest.TestCase):
@@ -8,22 +11,48 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Chrome("drivers/chromedriver.exe")
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
-        wd = self.wd
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").send_keys("secret")
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
+    def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").send_keys("na")
-        wd.find_element_by_name("group_header").send_keys("he")
-        wd.find_element_by_name("group_footer").send_keys("fo")
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
 
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group("name", "header", "footer"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group("", "", ""))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
