83fb06f5fcb098f9bfeb2591b2325b296ce11d37
anrepov <anrepov@gmail.com>
2021-05-03 19:59:30 +0300

    move files to directories

--- fixture/application.py
+++ fixture/application.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 class Application:
     def __init__(self):
-        self.wd = webdriver.Chrome("drivers/chromedriver.exe")
+        self.wd = webdriver.Chrome("../drivers/chromedriver.exe")
         self.wd.implicitly_wait(30)
 
     def return_to_home_page(self, wd):
