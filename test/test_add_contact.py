f893186124dc2f54f8712d395206e372fd14980c
anrepov <anrepov@gmail.com>
2021-05-21 22:11:19 +0300

    task 15 - parametrized tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
+import random
+import string
+
+import pytest
 
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    return prefix + "".join([random.choice(string.ascii_letters) for _ in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="")] + [
+    Contact(firstname=random_string("name", 10), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10))
+    for i in range(3)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contacts_list()
-    contact = Contact("name", "middlename", "lastname")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contacts_list()
