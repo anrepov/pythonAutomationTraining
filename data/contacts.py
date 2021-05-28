1238b3b011e2c1eecf43d5c4bc638172d2c7e434
anrepov <anrepov@gmail.com>
2021-05-28 16:30:28 +0300

    task 18 - add json test data generators

---
+++ data/contacts.py
@@ -0,0 +1,6 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="firstname1", middlename="middlename", lastname="lastname1"),
+    Contact(firstname="firstname2", middlename="middlename2", lastname="lastname2")
+]
