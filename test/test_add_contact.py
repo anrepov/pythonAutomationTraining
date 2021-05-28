b7fab69864579433b3909e97833fadf920eabef9
anrepov <anrepov@gmail.com>
2021-05-28 16:32:58 +0300

    task 19 - move tests data to files

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
-import random
-import string
-
-import pytest
 
 from model.contact import Contact
 
 
-def random_string(prefix, maxlen):
-    return prefix + "".join([random.choice(string.ascii_letters) for _ in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", middlename="", lastname="")] + [
-    Contact(firstname=random_string("name", 10), middlename=random_string("middlename", 10),
-            lastname=random_string("lastname", 10))
-    for i in range(3)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
