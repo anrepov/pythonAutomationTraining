436d4a7689a1e43bb46cf5b1d500e54ae4ee704e
anrepov <anrepov@gmail.com>
2021-06-10 17:07:22 +0300

    task 20 - fix error

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -16,5 +16,5 @@ def test_delete_some_contact(app, db, check_ui):
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max()) == sorted(app.contact.get_contacts_list(),
-                                                                       key=Contact.id_or_max())
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contacts_list(),
+                                                                     key=Contact.id_or_max)
